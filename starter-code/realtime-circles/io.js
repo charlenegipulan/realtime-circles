56e507dfffe4041de15bd4e438060a66539f77e6
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-01 11:43:22 -0700

    server code logic

--- starter-code/realtime-circles/io.js
+++ starter-code/realtime-circles/io.js
@@ -1,7 +1,10 @@
 var io = require('socket.io')();
 
 io.on('connection', function(socket) {
-    console.log('Client has connected to socket.io');
+
+    socket.on('add-circle', function(data) {
+        io.emit('add-circle', data);
+    });
 });
 
 module.exports = io;
